b331e60f70a4bcd05365a331de30ab5e3dfc7dfc
unknown <Mountain-Michaelle>
2024-04-26 12:49:49 +0100

    edited page.jxs

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,4 +1,4 @@
-import BlogCard from "@/components/BlogCard";
+import BlogCard from '../../components/BlogCard'
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
